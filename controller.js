a8437909a994b2fabf947455515b26a1733d249e
Hilarius Bayu <hilariuzbayu@outlook.com>
2020-04-11 13:54:53 +0700

    berhasil menampilkan semua data mahasiswa dalam database

--- controller.js
+++ controller.js
@@ -6,3 +6,15 @@ var connection = require('./koneksi')
 exports.index = function(req,res){
     response.ok("Aplikasi REST API berjalan cuy!",res)
 };
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('SELECT * FROM MAHASISWA', function(error, rows, field){
+        if(error){
+            connection.log(error);
+        }
+        else {
+            response.ok(rows, res)
+        }
+    });
+};
