844ae1dee9dc29403b745e8b79e041e0603d203a
Hilarius Bayu <hilariuzbayu@outlook.com>
2020-05-02 16:48:44 +0700

    rolee

--- middleware/verifikasi.js
+++ middleware/verifikasi.js
@@ -1,18 +1,20 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config/secret');
 
-function verifikasi(roles){
+function verifikasi(){
     return function(req, rest, next){
+        var role = req.body.role;
         //cek authorization header
         var tokenwithBearer = req.header.authorization;
         if(tokenwithBearer) {
             var token = tokenwithBearer.split (' ')[1];
+            
             //verifikasi
             jwt.verify(token, config.secret, function(err, decoded){
                 if(err){
                     return rest.status(401).send({auth:false, message:'Token tidak terdaftar!'});
                 }else {
-                    if(roles==2){
+                    if(role == 2){
                         req.auth = decoded;
                         next();
                     }else {
